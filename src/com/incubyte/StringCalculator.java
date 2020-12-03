c0e1b1e4800e280f2c041ad9cd049a8527850784
Yagya Singh Rajawat <yagya@iboxz.in>
2020-12-03 21:01:51 +0530

    refactor to two number string

--- src/com/incubyte/StringCalculator.java
+++ src/com/incubyte/StringCalculator.java
@@ -1,11 +1,23 @@
 package com.incubyte;
 
+import java.util.Arrays;
+
 public class StringCalculator {
     public int Add(String number) {
         if (number.equals("")) {
             return 0;
         } else {
-            return Integer.parseInt(number);
+            String[] num_s = number.split(",");
+            if (num_s.length == 1) {
+                return Integer.parseInt(num_s[0]);
+            } else {
+                int[] num_arr = new int[num_s.length];
+                for (int x=0; x< num_s.length; x++){
+                    num_arr[x] = Integer.parseInt(num_s[x]);
+                }
+                return Arrays.stream(num_arr).sum();
+            }
+
         }
     }
 }
