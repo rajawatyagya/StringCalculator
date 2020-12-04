0d1ceaac4c4744d9db161d5e4617236f5a186743
Yagya Singh Rajawat <yagya@iboxz.in>
2020-12-04 07:13:17 +0530

    negative error function refactor

--- src/com/incubyte/StringCalculator.java
+++ src/com/incubyte/StringCalculator.java
@@ -17,11 +17,15 @@ public class StringCalculator {
             }
             String[] num_s = number.split("[" + def_delimiter + "\n]");
             if (num_s.length == 1) {
-                return Integer.parseInt(num_s[0]);
+                if (Integer.parseInt(num_s[0]) < 0){
+                    throw new IllegalArgumentException("negatives not allowed, passed " + Integer.parseInt(num_s[0]));
+                } else return Integer.parseInt(num_s[0]);
             } else {
                 int[] num_arr = new int[num_s.length];
                 for (int x=0; x< num_s.length; x++){
-                    num_arr[x] = Integer.parseInt(num_s[x]);
+                    if (Integer.parseInt(num_s[x]) < 0){
+                        throw new IllegalArgumentException("negatives not allowed, passed " + Integer.parseInt(num_s[x]));
+                    } else num_arr[x] = Integer.parseInt(num_s[x]);
                 }
                 return Arrays.stream(num_arr).sum();
             }
